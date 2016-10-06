e38f665dbb3dd51f396e872df25473bbe9d5f3e2
= <vagrant@lighthouse-labs>
2016-10-06 22:36:17 +0000

    refactored to put the callback function inline

--- Week1/Day4/Callbacks/mapLyrics.js
+++ Week1/Day4/Callbacks/mapLyrics.js
@@ -1,6 +1,8 @@
-console.log(map(["ground", "control", "to", "major", "tom"], elm));
+console.log(map(["ground", "control", "to", "major", "tom"], function elm
+  (element){return element.length}));
 
-function map(array, fn){
+function map(array, fn)
+{
   var newArray = [];
   for (var element of array){
     newArray.push(fn(element));
@@ -8,7 +10,5 @@ function map(array, fn){
   return newArray;
 }
 
-function elm(element) {
-  return element.length;
-}
+
 
