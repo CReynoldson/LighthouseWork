636645c9afed229256132dd03b91a3d1e7fafc67
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-10-11 03:10:33 +0000

    refactored to avoid callback hell, create an avatar directory, and remove debugging console logs

--- Week2/GithubAPI/download_avatars.js
+++ Week2/GithubAPI/download_avatars.js
@@ -1,10 +1,13 @@
+//Grab necessary modules and commandline arguments
 var request = require("request");
 var fs = require("fs");
-var myArgs = process.argv.slice(2);
 
+
+//Main function for accessing Github endpoint
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  const endpoint = "https://api.github.com/repos/"+repoOwner+"/"+repoName+"/contributors?access_token=9455c1402a42a3147fe98138b1fae2cb1337f033";
+  //Create URL using user input from commandline
+  const endpoint = "https://api.github.com/repos/"+repoOwner+"/"+repoName+"/contributors";
   var options = {
     url: endpoint,
     headers: {
@@ -12,32 +15,39 @@ function getRepoContributors(repoOwner, repoName, cb) {
     },
     json: true,
   };
-
-  request(options, function(err, response, body){
-    if (err){
-      throw err;
-    }
-    cb(options, "tmp");
-  });
-}
-
-function downloadImageByURL(url, filepath){
-  request(url, function (err, response, body){
-    if (err){
-      throw err;
-    }
-    var urlArray = body.map(function(element, index, _array){
-      return element.avatar_url;
-    });
-    urlArray.forEach(function(element, index, _array){
-      var fileName = filepath + "/" + index + ".jpg";
-      request(element, function(err, response, body){
+  var directory = "./avatars/";
+  fs.stat(directory, function (err, stats){
+    if(err || !stats.isDirectory()){
+      fs.mkdir(directory, function(err) {
         if (err){
           throw err;
         }
-      }).pipe(fs.createWriteStream(fileName));
+      });
+    }
+  });
+
+  //Request Contributors List
+  request(options, function(err, response, body) {
+    if (err) {
+      throw err;
+    }
+    //Map each contributor object for the avatar_url and contributor username, send to callback function
+    body.map(function(contributorData) {
+      var fileName = contributorData.login + ".jpg";
+      cb(contributorData.avatar_url, (directory+fileName));
     });
   });
 }
 
+//Pipe data from avatar_url to a file directory
+function downloadImageByURL(url, filepath) {
+  request(url, function (err, response, body) {
+    if (err) {
+      throw err;
+    }
+  }).pipe(fs.createWriteStream(filepath));
+}
+
+//Get things started
+var myArgs = process.argv.slice(2);
 getRepoContributors(myArgs[0], myArgs[1], downloadImageByURL);
