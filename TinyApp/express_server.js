36dff4c164b5e4d576b233cff5216d43288871e5
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-10-12 20:46:02 +0000

    successful redirects to longURL

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -1,20 +1,19 @@
-var express = require("express");
-var app = express();
-app.set ("view engine", "ejs");
+//Declare Dependencies
+const express = require("express");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded());
+const app = express();
+
+//Declare Constants
 var PORT = process.env.PORT || 8080; // default port 8080
-
-
+app.set ("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-// app.get("/", (req, res) => {
-//   res.end("Hello!");
-// });
 
 app.get("/", (req, res) => {
   res.render("urls_new");
@@ -25,10 +24,14 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString(req);
+  let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(req.body);  // debug statement to see POST parameters
-  res.redirect("/urls/" + shortURL);         // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/" + shortURL); // this redirects to /urls/:id
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect("http://" + longURL);
 });
 
 app.get("/urls", (req, res) => {
@@ -36,17 +39,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+app.get("/urls/:id", (req, res) => { // this is where /urls/shortURL redirects to
+  let templateVars = { "shortURL": req.params.id };
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
