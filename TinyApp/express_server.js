410a8f9d436b8c803136f392853607b4f99b8e0a
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-10-13 21:58:43 +0000

    added username cookie functionality

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -1,28 +1,50 @@
 //Declare Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+const cookieParser = require("cookie-parser");
 
 //Declare Constants
+const app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 app.set ("view engine", "ejs");
+app.use(cookieParser());      //reads values from cookie
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const data = {
+  users: []
+};
+
 //"Home" page
 app.get("/", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 })
 
 //Clone of home page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
+});
+
+//Login page
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  console.log(req.body.username);
+  data.users.push({username: username});
+  res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/");
 });
 
 // Creates teenyURL and redirects to that teenyURL's specific page
@@ -49,13 +71,13 @@ app.get("/u/:shortURL", (req, res) => {
 // Displays urls index
 // It's hideous right now and needs formatting ***
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 // Displays specific teenyURL page
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { "shortURL": req.params.id, "longURL":urlDatabase[req.params.id] };
+  let templateVars = { username: req.cookies["username"], "shortURL": req.params.id, "longURL":urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
