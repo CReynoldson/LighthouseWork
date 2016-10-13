6bb4e8b16f74d512dc8f9c5425e850681e2148b8
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-10-13 18:35:20 +0000

    starting state before adding cookies feature

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -15,55 +15,79 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//"Home" page
 app.get("/", (req, res) => {
   res.render("urls_new");
 })
 
+//Clone of home page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Creates teenyURL and redirects to that teenyURL's specific page
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect("/urls/" + shortURL); // this redirects to /urls/:id
+  res.redirect("/urls/" + shortURL);
 });
 
+// Redirects from teenyURL to the corresponding longURL
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
-  res.redirect("http://" +longURL);
+  if(longURL.substring(0, 8) === "https://"){
+    longURL = longURL.substring(8);
+    res.redirect("http://" + longURL);
+  } else if (longURL.substring(0, 7) === "http://") {
+    longURL = longURL.substring(7);
+    res.redirect("http://" + longURL);
+  } else {
+    res.redirect("http://" + longURL);
+  }
 });
 
+// Displays urls index
+// It's hideous right now and needs formatting ***
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => { // this is where /urls/shortURL redirects to
-  let templateVars = { "shortURL": req.params.id };
+// Displays specific teenyURL page
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { "shortURL": req.params.id, "longURL":urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+// Updates urlDatabase and urls index
 app.post("/urls/:id", (req, res) => {
-  var newLongURL = req.body.longURL;
-  urlDatabase[req.params.id] = newLongURL;
-  res.redirect("/urls");
+  let newLongURL = req.body.longURL;
+  if (newLongURL.length === 0){
+    res.redirect("/urls");
+  } else {
+    urlDatabase[req.params.id] = newLongURL;
+    res.redirect("/urls");
+  }
 });
 
+// Deletes a url key:value pair from urlDatabase, redirects to url index
 app.post("/urls/:id/delete", (req, res) => {
   let deleteProperty = req.params.id;
   delete urlDatabase[deleteProperty];
   res.redirect("/urls");
 });
 
+// Delivers a .json formatted version of urlDatabase
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Make a port pay attention to you
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// Creates a random 6 character long string to act as a teenyURL
 function generateRandomString() {
   let charset = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let stringLength = 6;
@@ -74,4 +98,3 @@ function generateRandomString() {
   return randomString;
 }
 
-// console.log(generateRandomString());
